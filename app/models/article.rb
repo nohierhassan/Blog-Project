f4acfaa7e5d9b4029ba2f8d8b7666c1a56b0215b
Nohier <nohierassan2@gmail.com>
2018-10-14 17:07:45 +0200

    adding user-association Successfully

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ApplicationRecord
+  belongs_to :user
   # validation for the existence of the title
   validates :title, presence: true, length: {minimum: 3, maximum: 50}
   validates :description, presence: true
+  validates :user_id, presence: true
 end
